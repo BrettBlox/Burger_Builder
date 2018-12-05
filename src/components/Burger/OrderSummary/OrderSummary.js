2aa20b01c8df29849e31af1ad0f35c7329b024a1
BrettBlox <Diamond5.12>
2018-12-05 11:15:08 -0700

    changed modal to class component and added lifecycle methods

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(el => {
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(el => {
+      return (
+        <li key={el}>
+          <span style={{ textTransform: "capitalize" }}>{el}</span>
+        </li>
+      );
+    });
     return (
-      <li key={el}>
-        <span style={{ textTransform: "capitalize" }}>{el}</span>
-      </li>
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Your Burger Ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <strong>
+          <p>Total Price: {this.props.price.toFixed(2)}</p>
+        </strong>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>
+          CONTINUE
+        </Button>
+      </Aux>
     );
-  });
+  }
+}
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Your Burger Ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <strong>
-        <p>Total Price: {props.price.toFixed(2)}</p>
-      </strong>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
