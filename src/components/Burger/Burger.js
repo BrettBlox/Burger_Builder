71735b524d89d68abe59adeb6c2547b806fe9482
BrettBlox <Diamond5.12>
2018-12-04 17:46:03 -0700

    burgergit a .git a .

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(el => {
+      return [...Array(props.ingredients[el])].map((_, i) => {
+        return <BurgerIngredient key={el + i} type={el} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Start Adding Burger Ingredients!</p>
+    }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
