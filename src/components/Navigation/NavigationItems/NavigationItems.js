4e6016ab3e7fbd86282243f3b4186ef5adc43b12
BrettBlox <Diamond5.12>
2018-12-05 11:01:24 -0700

    updated menu

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>
+    <NavigationItem link="/" active={true}>
       Burger Builder
     </NavigationItem>
     <NavigationItem link="/">Checkout</NavigationItem>
